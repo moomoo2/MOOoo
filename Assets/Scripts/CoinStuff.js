0d38c70f169c3519f6dcbaff4ec64656b82d69fe
Tessa Mazzarella <tessamazzarella@gmail.com>
2016-09-13 11:16:00 -0400

    ball and weight race

---
+++ Assets/Scripts/CoinStuff.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+public var points : int = 5; //we have a variable points equal to 5
+public var pickedUpBy : String = "Player"; //we have variable pickedUpBy equal to player
+
+function OnTriggerEnter2D(other : Collider2D)// in this function our parameter is other it si of type Collider2D
+{
+	if ( other.CompareTag(pickedUpBy) )
+	{
+		Debug.Log("WOW! Worth " + points + " points!");
+
+		Destroy(gameObject);
+
+	}
+}
